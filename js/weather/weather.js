fc33eaa65cee9b522ab0c4e3d7952ef04905a32b
konstanchuk <vova.konstanchuk@gmail.com>
2014-11-26 21:49:04 +0200

    1

--- js/weather/weather.js
+++ js/weather/weather.js
@@ -1,3 +1,41 @@
+function WeatherApi() {
+    var key = '5014b616aa426e0a5a01ccf7ebc3d';
+
+    this.getWeather = function(funcSuccess, funcError) {
+        $.ajax({
+            url: 'http://api.worldweatheronline.com/free/v2/weather.ashx?q=London&format=json&num_of_days=5&key=' + key,
+            type: 'GET',
+            contentType: 'application/json',
+            success: function (response) {
+                if (response) {
+                    funcSuccess(response);
+                }
+            },
+            error: funcError
+        });
+    }
+}
+
+function WeatherModel() {
+
+    var self = this;
+    self.weather_days = ko.observableArray([]);
+
+    function renderData(data) {
+        var weather = data.data.weather;
+        alert(weather.length);
+        for(var i in weather) {
+            self.weather_days.push({
+                date: weather[i].date
+            })
+        }
+    }
+
+
+    var api = new WeatherApi();
+    api.getWeather(renderData, function() { alert('error'); });
+}
+
 $(document).ready(function() {
-    alert(1);
+    ko.applyBindings(new WeatherModel(), document.getElementById('weather-list'));
 });
