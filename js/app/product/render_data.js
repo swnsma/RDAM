b4caa664ea7f846d2fc5edfc318cbc377e4c1a32
Borys <mbl171495@gmail.com>
2014-11-30 01:42:31 +0200

    move getData to common

--- js/app/product/render_data.js
+++ js/app/product/render_data.js
@@ -90,3 +90,25 @@ function render(item) {
             seriesColors: ['green']
         });
 }
+
+function Product() {
+    getData(render);
+
+    setTimeout(function() {
+        changeGraph(1);
+    }, 1000);
+
+    var $target = $('.graphContainer');
+    var $dayButton = $('<div>');
+    var $weekButton = $('<div>');
+    var $monthButton = $('<div>');
+
+    button_constr($dayButton, $target, 1,'Days');
+    button_constr($weekButton, $target, 2,'Weeks');
+    button_constr($monthButton, $target, 3,'Months');
+
+}
+
+$(document).ready(function() {
+    new Product();
+});
