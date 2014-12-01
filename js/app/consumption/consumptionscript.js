99af25de7d1c9d0f70cddab70918acbc44e55a1d
AndreyUser <vfreiby@gmail.com>
2014-12-02 00:26:49 +0200

    тепер батарейки будує нокаут. Аве нокаут!!!

--- js/app/consumption/consumptionscript.js
+++ js/app/consumption/consumptionscript.js
@@ -1,14 +1,98 @@
-function Consumtion() {
-    var allData=[];
+$(document).ready(function() {
 
-    var dayProduction;
-    var dayConsumption;
-    var weekProduction;
-    var weekConsumption;
-    var monthProduction;
-    var monthConsumption;
+    ko.bindingHandlers.battery = {
+        init: function(element, valueAccessor) {
+            // This will be called when the binding is first applied to an element
+            // Set up any initial state, event handlers, etc. here
 
-    function getData() {
+            var value = valueAccessor();
+
+
+            var ctx = element.getContext('2d');
+            var consumption = value.consumption;
+            var production = value.production;
+
+            var a = 550;
+
+            var b = 150;
+            if (production <= consumption) {
+                var percentage = production / consumption;
+            }
+            else {
+                percentage = 1;
+            }
+
+            element.width = a;
+            element.height = b;
+
+            var batteryWidth = a - 200;
+            var batteryHeight = element.height;
+
+            ctx.fillStyle = "#8BC34A";
+            ctx.lineWidth=2;
+            ctx.strokeRect(100, 0, batteryWidth * percentage, batteryHeight);
+            ctx.fillRect(100, 1, batteryWidth * percentage, batteryHeight-2);
+
+            ctx.fillStyle = "#FF8A65 ";
+            ctx.strokeRect(batteryWidth * percentage+100, 0, batteryWidth - batteryWidth * percentage, batteryHeight);
+            ctx.fillRect(batteryWidth * percentage+100, 1, batteryWidth - batteryWidth * percentage, batteryHeight-2);
+
+
+            ctx.beginPath();
+            ctx.arc(101,batteryHeight/2,batteryHeight/2-1,0.5*Math.PI,Math.PI*1.5);
+            ctx.stroke();
+            ctx.fillStyle = "#8BC34A";
+            ctx.fill();
+
+
+            ctx.beginPath();
+            ctx.arc(batteryWidth*percentage+101,batteryHeight/2,batteryHeight/2-1,0.5*Math.PI,Math.PI*1.5);
+            ctx.stroke();
+            ctx.fillStyle = "#FF8A65";
+            ctx.fill();
+
+            ctx.beginPath();
+            ctx.arc(batteryWidth+100,batteryHeight/2,batteryHeight/2-1,0,Math.PI*2);
+            ctx.stroke();
+            ctx.fillStyle = "#F4EFD4";
+            ctx.fill();
+
+            ctx.beginPath();
+            ctx.arc(batteryWidth+100,batteryHeight/2,15,0,Math.PI*2);
+            ctx.stroke();
+            ctx.fillStyle = "black";
+            ctx.fill();
+
+            ctx.beginPath();
+            ctx.arc(batteryWidth+93,batteryHeight/2,15,0,Math.PI*2);
+            ctx.stroke();
+
+
+        },
+        update: function(element, valueAccessor) {
+            // This will be called once when the binding is first applied to an element,
+            // and again whenever any observables/computeds that are accessed change
+            // Update the DOM element based on the supplied values here.
+        }
+    };
+
+    var arr=[];
+
+    var successFunction=function(someData){
+        for(var i=0;i<someData.length;i++){
+            arr.push(someData[i]);
+        }
+
+
+        var viewModelConstructor = function(data){
+            this.batteries = data;
+        };
+
+
+        ko.applyBindings(new viewModelConstructor(arr));
+    };
+
+    function getData(successFunction) {
         var options = {
             url: 'https://api.parse.com/1/classes/consumption_data/',
             type: 'GET',
@@ -19,23 +103,7 @@ function Consumtion() {
             },
             success: function (response) {
                 if (response && response.results && response.results.length) {
-
-                    for (var i = 0; i < response.results.length; i++) {
-                        var a = response.results[i];
-                        allData.push(a);
-                    }
-                    dayProduction = allData[1].production;
-                    dayConsumption = allData[1].consumption;
-                    batteryRender('dayBattery', dayProduction, dayConsumption, 'Yesterday');
-
-                    weekProduction=allData[0].production;
-                    weekConsumption= allData[0].consumption;
-                    batteryRender('weekBattery', weekProduction, weekConsumption, 'Last week');
-
-                    monthProduction=allData[2].production;
-                    monthConsumption= allData[2].consumption;
-                    batteryRender('monthBattery', monthProduction, monthConsumption, 'Last month');
-
+                    successFunction(response.results);
                 }
             },
 
@@ -44,129 +112,7 @@ function Consumtion() {
         };
         $.ajax(options);
     };
-    getData();
 
-    var batteryRender = function (id, production, consumption) {
+    getData(successFunction);
 
-        var $target = $('.batteryContainer');
-        var $width = $target.css('width');
-        var a = $width.substr(0, $width.length - 2);
-
-        var $height = $target.css('height');
-        var b = $height.substr(0, $height.length - 2);
-        if (production <= consumption) {
-            var percentage = production / consumption;
-        }
-        else {
-            percentage = 1;
-        }
-        var example = document.getElementById(id),
-            ctx = example.getContext('2d');
-
-        example.width = a;
-        example.height = b;
-
-        var batteryWidth = a - 200;
-        var batteryHeight = example.height;
-
-
-        if(percentage!==0) {
-            var pic       = new Image();
-            pic.src    = 'img/solna15.png';
-            pic.onload = function() {
-                ctx.drawImage(pic, 10,50);
-            }};
-//
-
-        ctx.fillStyle = "#71BF44";
-        ctx.lineWidth=2;
-        ctx.strokeRect(100, 0, batteryWidth * percentage, batteryHeight);
-        ctx.fillRect(100, 1, batteryWidth * percentage, batteryHeight-2);
-
-
-
-
-
-        ctx.fillStyle = "#BF4444 ";
-        ctx.strokeRect(batteryWidth * percentage+100, 0, batteryWidth - batteryWidth * percentage, batteryHeight);
-        ctx.fillRect(batteryWidth * percentage+100, 1, batteryWidth - batteryWidth * percentage, batteryHeight-2);
-
-
-        if(percentage!==1) {
-            var pic2      = new Image();
-            pic2.src    = 'img/31863.png';
-            pic2.onload = function() {
-                ctx.drawImage(pic2, 460,50);
-            };
-        }
-        //ctx.scale(1,2);
-
-
-
-        ctx.beginPath();
-        ctx.arc(101,batteryHeight/2,batteryHeight/2-1,0.5*Math.PI,Math.PI*1.5);
-        ctx.stroke();
-        ctx.fillStyle = "#71BF44";
-        ctx.fill();
-
-
-        ctx.beginPath();
-        ctx.arc(batteryWidth*percentage+101,batteryHeight/2,batteryHeight/2-1,0.5*Math.PI,Math.PI*1.5);
-        ctx.stroke();
-        ctx.fillStyle = "#BF4444";
-        ctx.fill();
-
-        ctx.beginPath();
-        ctx.arc(batteryWidth+100,batteryHeight/2,batteryHeight/2-1,0,Math.PI*2);
-        ctx.stroke();
-        ctx.fillStyle = "#F4EFD4";
-        ctx.fill();
-
-        ctx.beginPath();
-        ctx.arc(batteryWidth+100,batteryHeight/2,30,0,Math.PI*2);
-        ctx.stroke();
-        ctx.fillStyle = "black";
-        ctx.fill();
-
-        ctx.beginPath();
-        ctx.arc(batteryWidth+90,batteryHeight/2,30,0,Math.PI*2);
-        ctx.stroke();
-
-        //ctx.beginPath();
-        //ctx.arc(batteryWidth+100,batteryHeight/2,20,0,Math.PI*2);
-        //ctx.stroke();
-        //ctx.fillStyle = "black";
-        //ctx.fill();
-
-        //  ctx.fillStyle = "yellow";
-        //  ctx.font = 'bold 30px sans-serif';
-        //  ctx.fillText("Total consumption "+ consumption+" kWatt", 140, 110);
-        //  ctx.strokeStyle='black';
-        //  ctx.strokeText("Total consumption "+ consumption+" kWatt", 140, 110);
-        //  ctx.strokeStyle='black';
-
-        //$target = document.getElementById(id);
-
-        //  var $target1 = $target.parentNode;
-        //  //console.log($target1);
-
-        //  var taken = consumption - production;
-
-        //  var $prodSpan = $('<div>');
-        //  $prodSpan.appendTo($target1).addClass('prodSpan').addClass('prod').text(day + ' production ' + production + ' kWatts.');
-        //  if (percentage !== 1) {
-        //      var $consSpan = $('<div>');
-        //      $consSpan.appendTo($target1).addClass('prodSpan').addClass('cons').text(day + ' taken from grid ' + taken + ' kWatts.');
-        //  }
-        //  if(percentage==1)
-        //  {
-        //       $consSpan = $('<div>');
-        //      $consSpan.appendTo($target1).addClass('prodSpan').addClass('cons').text(day + ' consumption ' + consumption + ' kWatts.');
-        //  }
-    };
-
-}
-
-$(document).ready(function() {
-    new Consumtion();
 });
