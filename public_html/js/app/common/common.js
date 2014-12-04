af1d869af12d7a5494a1f693fb0ce5f87b2216b6
Borys <mbl171495@gmail.com>
2014-12-04 23:34:16 +0200

    add options to separate file

--- public_html/js/app/common/common.js
+++ public_html/js/app/common/common.js
@@ -86,72 +86,12 @@ function LoadingManager() {
     })();
 }
 
-var options={
-    seriesDefault: {
+function includeJs(jsFilePath) {
+    var js = document.createElement("script");
 
-        showMarker:false
-    },
-    series: [
-        {
-            rendererOptions: {
-                fillToZero: true
-            },
-            shadow: false,
-            label:"consumption",
+    js.type = "text/javascript";
+    js.src = jsFilePath;
 
-            markerOptions: {shadow:false
-            }
-        },
-        {
-            rendererOptions: {
-                fillToZero: false
-            },
-            shadow: false,
-            label:"production",
-            markerOptions: {shadow:false}
-
-        }
-    ],
-    highlighter: {show: true, sizeAdjust: 25,
-        tooltipAxes:"y",
-        bringSeriesToFront:true
-    },
-    legend: {
-        show: true,
-        location: 'ne',     // compass direction, nw, n, ne, e, se, s, sw, w.
-        xoffset: 12,        // pixel offset of the legend box from the x (or x2) axis.
-        yoffset: 12        // pixel offset of the legend box from the y (or y2) axis.
-    },
-    axes: {
-
-        xaxis: {
-            renderer: $.jqplot.CategoryAxisRenderer,
-            tickOptions: {formatString: '%b %#d, %y'},
-            tickInterval: '1 month',
-            pad:1.05
-        },
-
-        yaxis: {
-            show:false,
-            label:"kW",
-            pad: 1.001,
-            tickOptions: {formatString: '%d'}
-        }
-    },
-    cursor: {show: true,
-        showHorizontalLine:true,
-        zoom:true,
-        clickReset:true},
-    grid: {
-        drawGridLines: false,
-        gridLineColor: '#fff',
-        background: '#eee',
-        borderColor: '#eee',
-        borderWidth: 1,
-        shadow: false,
-        renderer: $.jqplot.CanvasGridRenderer,
-        rendererOptions: {}
-    }
-    //seriesColors: colors
-};
+    document.body.appendChild(js);
+}
 var loading = new LoadingManager();
