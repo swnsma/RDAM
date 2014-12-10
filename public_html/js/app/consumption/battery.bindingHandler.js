f4d9fc482c844be7065dc0713c95528d93d1a850
konstanchuk <vova.konstanchuk@gmail.com>
2014-12-10 16:53:38 +0200

    afrrgf

--- public_html/js/app/consumption/battery.bindingHandler.js
+++ public_html/js/app/consumption/battery.bindingHandler.js
@@ -6,8 +6,8 @@ ko.bindingHandlers.battery = {
         var ctx = element.getContext('2d');
         var consumption = value.consumption;
         var production = value.production;
-        var a = 500;
-        var b = 200;
+        var a = 400;
+        var b = 120;
         if (production <= consumption) {
             var percentage = production / consumption;
         }
@@ -25,11 +25,11 @@ ko.bindingHandlers.battery = {
         ctx.lineWidth=2;
         ctx.fillRect(0, 0, batteryWidth * percentage, batteryHeight);
 
-        ctx.fillStyle = "#F44336";
+        ctx.fillStyle = 'rgb(244, 239, 212)';
 
         ctx.fillRect(batteryWidth * percentage, 0, batteryWidth - batteryWidth * percentage, batteryHeight);
-        ctx.font = "bold 28px sans-serif";
+        ctx.font = "bold 20px sans-serif";
         ctx.fillStyle='black';
-        ctx.fillText(value.date2+' consumption '+consumption+' kWatts',15,100)
+        ctx.fillText(value.date2+' consumption '+consumption+' kWh',40,60)
     }
 };
