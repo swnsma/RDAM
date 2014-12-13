09c9512521e12e5208517630989872981947eb47
AndreyUser <vfreiby@gmail.com>
2014-12-13 14:56:27 +0200

    убрав канвас

--- public_html/js/app/consumption/battery.bindingHandler.js
+++ public_html/js/app/consumption/battery.bindingHandler.js
@@ -3,33 +3,13 @@ ko.bindingHandlers.battery = {
 
         var value = valueAccessor();
 
-        var ctx = element.getContext('2d');
         var consumption = value.consumption;
         var production = value.production;
-        var a = 400;
-        var b = 120;
-        if (production <= consumption) {
-            var percentage = production / consumption;
-        }
-        else {
-            percentage = 1;
-        }
 
-        element.width =a;
-        element.height = b;
+        var percentage=production/consumption*100;
 
-        var batteryWidth =element.width;
-        var batteryHeight = element.height;
 
-        ctx.fillStyle = "#4CAF50";
-        ctx.lineWidth=2;
-        ctx.fillRect(0, 0, batteryWidth * percentage, batteryHeight);
-
-        ctx.fillStyle = 'rgb(244, 239, 212)';
-
-        ctx.fillRect(batteryWidth * percentage, 0, batteryWidth - batteryWidth * percentage, batteryHeight);
-        ctx.font = "bold 20px sans-serif";
-        ctx.fillStyle='black';
-        ctx.fillText(value.date2+' consumption '+consumption+' kWh',40,60)
+        element.style.width=percentage+'%';
+        //element.innerText='adsdafads'
     }
 };
