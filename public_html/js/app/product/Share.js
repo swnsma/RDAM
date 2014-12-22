0747667ff7c1bab4f547b581cf17a35d071ffcbe
swnsma <swnsma@gmail.com>
2014-12-22 20:46:03 +0200

    1) Исправлен шеринг. Ждет теста

--- public_html/js/app/product/Share.js
+++ public_html/js/app/product/Share.js
@@ -10,13 +10,15 @@
         }
     }
     $('.fb-share-button', '#fb-root').attr({'data-href':window.location.href});
+    fb(document, 'script', 'facebook-jssdk');
 })();
 
-    (function(d, s, id) {
+    function fb(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
     js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
     fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+}
+
 
