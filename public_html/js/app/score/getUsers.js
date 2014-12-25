1ec44990735886216d46f1f7d8c3f8701b0763d6
konstanchuk <vova.konstanchuk@gmail.com>
2014-12-25 22:38:22 +0200

    під нову базу

--- public_html/js/app/score/getUsers.js
+++ public_html/js/app/score/getUsers.js
@@ -1,6 +1,6 @@
 function getUsers(self) {
     var options = {
-        url: 'http://rdam.zz.mu/ajax/users_info.php?from_id=' + 1  + '&fields=rating',
+        url: 'http://rdam.tk/ajax/users_info.php?from_id=' + 1,
         type: 'GET',
         contentType: 'application/json',
         success: function (response) {
@@ -21,7 +21,7 @@ function analyze(response,self){
             if(response.data[i].id!=self.current_user().id()) {
                 var a = response.data[i];
                 var j = i % masColor.length;
-                var b = new Users(a.user, a.id, false, masColor[j],a.rating);
+                var b = new Users(a.user, a.id, false, masColor[j]);
 
                 mappedTasks.push(b);
             }
