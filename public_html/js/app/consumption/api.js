8efc11c88f8c9aa73757dd7f8731546c5af7de53
AndreyUser <vfreiby@gmail.com>
2014-12-28 03:07:48 +0200

    підключив базу даних

--- public_html/js/app/consumption/api.js
+++ public_html/js/app/consumption/api.js
@@ -1,23 +1,23 @@
 var api = {
-    getData: function(successFunction){
-        var options = {
-            url: 'https://api.parse.com/1/classes/consumption_data/',
-            type: 'GET',
-            contentType: 'application/json',
-            beforeSend: function (request) {
-                request.setRequestHeader('X-Parse-Application-Id', 'dEruvPYiXg1FAzhn4u47ZP8Yjd7B2Ss6Gqjqi7h3');
-                request.setRequestHeader('X-Parse-REST-API-Key', 'a6npew12pgZaQJSTeCtPru3cVGS9VmZzG1op4mK8');
-            },
-            success: function (response) {
-                loading.disable();
-                if (response && response.results && response.results.length) {
-                    successFunction(response.results);
-                }
-            },
+    getData: getValues = function (id,successFunction, type) {
+            $.ajax({
+                url: 'http://rdam.tk/ajax/users_values.php?id=' + id + '&todt=last' + type,
+                type: 'GET',
+                contentType: 'application/json',
 
-            error: function () {
-            }
-        };
-        $.ajax(options);
-    }
+                success: function (response) {
+                    if (response) {
+                        successFunction(response);
+                        loading.disable()
+                     }
+                    else {
+                      alert('2')
+                    }
+                },
+                error: function (xhr, status, error) {
+                    var err = JSON.parse(xhr.responseText);
+
+            }});
+        }
+
 }
