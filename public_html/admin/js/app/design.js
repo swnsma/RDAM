0469bc35044f1ad954df172325864d109e788349
konstanchuk <vova.konstanchuk@gmail.com>
2015-01-05 23:32:49 +0200

    2014-2015

--- public_html/admin/js/app/design.js
+++ public_html/admin/js/app/design.js
@@ -2,9 +2,40 @@ function Desing() {
     var self = this;
     self.templates = ko.observableArray([]);
 
+    self.upload_template = function() {
+        ajax.load_skin(new FormData(document.getElementById('uploadTemplate')), {
+            success: function() {
+
+            },
+            error: function() {
+
+            },
+            after: function() {
+
+            },
+            before: function() {
+
+            }
+        })
+    };
+
+    self.delete_template = function(data) {
+
+        //...
+
+        return false;
+    };
+
+    self.select_design = function(data) {
+        if (!data.active) return false;
+
+        //....
+
+        return true;
+    };
+
     ajax.list_skins({
         success: function(data) {
-            console.log(data);
             for(var i in data) {
                 self.templates.push(new Template(data[i]));
             }
@@ -22,12 +53,12 @@ function Desing() {
 }
 
 function Template(data) {
+    this.id = data.id;
     this.name = data.name;
     this.author = data.author;
     this.version = data.version;
     this.comment = data.comment;
     this.active = data.active == 1 ? true : false;
-
 }
 
 $(document).ready(function() {
