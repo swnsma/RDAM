5e72e8779bac441b9a349aab9f13a77aa8abbfab
Таня <taaania@bk.ru>
2015-01-06 23:59:49 +0200

    admin:  1)new:   1.1) підказка

--- public_html/admin/js/app/validator.js
+++ public_html/admin/js/app/validator.js
@@ -2,10 +2,11 @@ function Validator() {}
 
 Validator.prototype.user_name = function(name) {
     return /^[\w \.\'\-]{1,25}$/.test(name);
+//    return /^[\w \n\(\)\'\"\d\-]{1,25}$/.test(name);
 };
 
 Validator.prototype.city = function(city) {
-    return /^[\w \-]{5,25}$/.test(city);
+    return /^[\w \-]{2,25}$/.test(city);
 };
 
 Validator.prototype.descr = function(descr) {
