43f884d8f8877350ae18b65e3938e66acd91df84
konstanchuk <vova.konstanchuk@gmail.com>
2015-01-12 16:54:05 +0200

    +

--- conf/conf.php
+++ conf/conf.php
@@ -1,15 +1,26 @@
 <?php
 
+define('DB_SERVER', 'localhost');
+define('DB_NAME', 'rte');
+define('DB_USER', 'rte');
+define('DB_PASSWORD', '3Z84QaJb');
+
+define('DB_SERVER_V', 'localhost');
+define('DB_NAME_V', 'rte');
+define('DB_USER_V', 'rte');
+define('DB_PASSWORD_V', '3Z84QaJb');
+
+/*
 ##################################################
 
 #db
 define('DB_SERVER', 'localhost');
 define('DB_NAME', 'rdam');
 
-//define('DB_USER', 'root');
-define('DB_USER', 'mysql');
-//define('DB_PASSWORD', 'root');
-define('DB_PASSWORD', 'mysql');
+define('DB_USER', 'root');
+//define('DB_USER', 'mysql');
+define('DB_PASSWORD', 'root');
+//define('DB_PASSWORD', 'mysql');
 
 ##################################################
 
@@ -17,14 +28,14 @@ define('DB_PASSWORD', 'mysql');
 define('DB_SERVER_V', 'localhost');
 define('DB_NAME_V', 'rdam_values');
 
-//define('DB_USER_V', 'root');
-define('DB_USER_V', 'mysql');
+define('DB_USER_V', 'root');
+//define('DB_USER_V', 'mysql');
 
-//define('DB_PASSWORD_V', 'root');
-define('DB_PASSWORD_V', 'mysql');
+define('DB_PASSWORD_V', 'root');
+//define('DB_PASSWORD_V', 'mysql');
 
 ##################################################
-
+*/
 define('TIME_ZONE', 'Europe/London');
 
 define('LOG_FILE', '../logs/log.txt');
