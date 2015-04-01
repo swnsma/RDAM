e45cd2cfc0b1298e70bb37300414ff64d72b25dd
Таня <taaania@bk.ru>
2015-04-01 18:00:47 +0300

    Добавлений title

--- spa/js/app/bindings.js
+++ spa/js/app/bindings.js
@@ -116,3 +116,8 @@ ko.bindingHandlers.sumCar={
     }
 };
 
+ko.bindingHandlers.title = {
+    init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+            $('title').text(viewModel.userName() + "| RTE");
+    }
+}
--- spa/js/app/main.js
+++ spa/js/app/main.js
@@ -4,6 +4,7 @@ var main=function() {
 
     var modelHome= new AppViewModel('chart_home',values.getProCon,true,'',$(".table-metrics"),$(".graph-container"));
     modelHome.activate();
-    ko.applyBindings(modelHome, document.getElementById('body'));
+    ko.applyBindings(modelHome);
+
 };
 manager.add(main);
