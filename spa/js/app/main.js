58af930108f75a5719b5e25a821e01cbce0776a9
swnsma <swnsma@gmail.com>
2015-04-08 17:03:36 +0300

    1)Fixed

--- spa/js/app/main.js
+++ spa/js/app/main.js
@@ -8,6 +8,10 @@ function removeLoad(){
         }, 1000);
     },500);
 }
+function errorMessage(){
+    $('#error').css({'display':'block'});
+    $('#load').css({'display':'none'});
+}
 $(document).ready( function() {
         $('.menu').smint({
             'scrollSpeed' : 1000
@@ -29,9 +33,11 @@ $(document).ready( function() {
             }).done(function(){
                 window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.twttr||{};if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);t._e=[];t.ready=function(f){t._e.push(f);};return t;}(document,"script","twitter-wjs"));
                 removeLoad();
-            });
+            })
         }
-        )
+        ).fail(function(){
+            errorMessage();
+        });
 });
 $(window).bind('hashchange', function() {
         location.reload();
